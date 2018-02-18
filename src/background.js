0c489ad6d4a1464cf8c8c2a3191390398e5fcfdb
Nicholas Palenchar <nickpalenchar@users.noreply.github.com>
2018-02-18 11:45:58 -0500

    chpt

--- src/background.js
+++ src/background.js
@@ -8,15 +8,39 @@ function getActiveTab(fn) {
   });
 }
 
-function onResponse(message) {
+function browserClickResponse (message) {
   console.log('message recieved ', message);
-  alert('git eet');
+  getActiveTab(function(activeTab) {
+    chrome.tabs.sendMessage(activeTab.id, message);
+  })
+}
 
+function nukeRound(message) {
+  // handles a nuke round, which is the total deletion of one type of facebook resource
+  // (i.e. timeline, photos, etc). nukeRound is a response callback that can also be invoked
+  // to begin deletion.
+  //
+  // Pops message.nuke array and begins deletion on the string it gets back,
+  // then recursively calls itself with the popped array, to get the next value of
+  // resoure to delete. Continues until array is empty.
+
+  var toNuke = message.nuke.pop();
+
+  getActiveTab(function(activeTab) {
+    chrome.tabs.sen
+  });
 }
 
 chrome.browserAction.onClicked.addListener(function(tab) {
   // Send a message to the active tab
   getActiveTab(function(activeTab){
-    chrome.tabs.sendMessage(activeTab.id, { "message": "clicked_browser_action" }, onResponse);
+    chrome.tabs.sendMessage(activeTab.id, { "message": "clicked_browser_action" }, browserClickResponse);
   })
 });
+
+function sendMessageToActiveTab(message, responseFn) {
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    var activeTab = tabs[0];
+    chrome.tabs.sendMessage(activeTab.id, message, responseFn);
+  });
+}
