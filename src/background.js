5834a4221152af8cd43fe5b9ac6eb9f6bd559814
Nicholas Palenchar <nickpalenchar@users.noreply.github.com>
2018-02-18 11:36:31 -0500

    works

--- src/background.js
+++ src/background.js
@@ -1,5 +1,5 @@
 console.log('[background.js] v10');
-alert('v10')
+alert('v10');
 
 function getActiveTab(fn) {
   // callback function has the active tab object
@@ -10,12 +10,13 @@ function getActiveTab(fn) {
 
 function onResponse(message) {
   console.log('message recieved ', message);
+  alert('git eet');
 
 }
 
 chrome.browserAction.onClicked.addListener(function(tab) {
   // Send a message to the active tab
   getActiveTab(function(activeTab){
-    chrome.tabs.sendMessage(activeTab.id, { "message": "clicked_browser_action" });
+    chrome.tabs.sendMessage(activeTab.id, { "message": "clicked_browser_action" }, onResponse);
   })
 });
